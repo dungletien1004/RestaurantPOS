64ded26a8388b1f7a424efb9651eb67110a9f13e
Duong Hanh Minh <hminh.dg@gmail.com>
2021-11-06 21:14:55 +0700

    Add Checkout

---
+++ app/src/components/checkout.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Checkout extends React.Component {
+    
+    render() { 
+    return <div className="card" style={{width: '18rem'}}>
+        <p>Payment view</p>
+      </div>
+      ;
+    }
+}
+ 
+export default Checkout;
