d00ee7aef7f7f2ad4b76e9a4c272b184ce0a584a
Duong Hanh Minh <hminh.dg@gmail.com>
2021-11-14 19:15:07 +0700

    Add Payment Controller

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,31 @@
+import React, { useState } from 'react';
 import Foods from './components/foodList';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Checkout from './components/CheckoutForm/Checkout/Checkout';
+import { commerce } from './lib/commerce';
 function App() {
+  const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('',);
+
+  // const refreshCart = async() => {
+  //   const newCart = await commerce.cart.refresh();
+  //   setCart(newCart);
+  // }
+
+  const handleCaptureCheckout = async (checkoutTokenID, newOrder) => {
+    try{
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenID, newOrder);
+      setOrder(incomingOrder);
+      // refreshCart(); 
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
+    }
+  }
+
   return (
+    <>
     <div className="App">
       <Header />
         <p></p>
@@ -10,6 +33,14 @@ function App() {
         <p></p>
       <Footer />
     </div>
+
+    {/* <Checkout 
+      cart={cart} 
+      order={order}
+      onCaptureCheckout={handleCaptureCheckout}
+      error={errorMessage}  
+    /> */}
+    </>
   );
 }
 
